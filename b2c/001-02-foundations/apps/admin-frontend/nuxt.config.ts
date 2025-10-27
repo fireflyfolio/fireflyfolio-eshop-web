5d87a53c30373aedf63d4603f6410fa1ff9d47ea
kenshirobb <kenshirobb@localhost>
2025-10-27 23:57:25 +0100

    refactor: fix nuxt crash

--- b2c/001-02-foundations/apps/admin-frontend/nuxt.config.ts
+++ b2c/001-02-foundations/apps/admin-frontend/nuxt.config.ts
@@ -1,9 +1,7 @@
 // Nuxt 3 minimal SSR
 export default defineNuxtConfig({
   devtools: false,
-  nitro: { preset: 'node' },
   typescript: { strict: true },
-
   runtimeConfig: {
     // Environnements à l’exécution (serveur uniquement)
     INTERNAL_ADMIN_API_URL: process.env.INTERNAL_ADMIN_API_URL || 'http://admin-api:3001'
