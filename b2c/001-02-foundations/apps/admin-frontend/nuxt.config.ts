4d9b588cf584a4dd8cbd0ab207a62f539a871072
kenshirobb <kenshirobb@localhost>
2025-10-27 11:20:33 +0100

    refactor: fix nuxt config

--- b2c/001-02-foundations/apps/admin-frontend/nuxt.config.ts
+++ b2c/001-02-foundations/apps/admin-frontend/nuxt.config.ts
@@ -3,8 +3,11 @@ export default defineNuxtConfig({
   devtools: false,
   nitro: { preset: 'node' },
   typescript: { strict: true },
+
   runtimeConfig: {
     // Environnements à l’exécution (serveur uniquement)
     INTERNAL_ADMIN_API_URL: process.env.INTERNAL_ADMIN_API_URL || 'http://admin-api:3000'
-  }
+  },
+
+  compatibilityDate: '2025-10-27'
 });
