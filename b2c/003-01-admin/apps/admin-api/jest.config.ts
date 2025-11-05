07fa2e836e9e5df491755ec13a6d7128ab692323
kenshirobb <kenshirobb@localhost>
2025-11-05 11:46:07 +0100

    chore: prepare admin story

---
+++ b2c/003-01-admin/apps/admin-api/jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  testMatch: ['**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: { '^.+\\.ts$': ['ts-jest', { tsconfig: './tsconfig.json' }] }
+};
+
+export default config;
